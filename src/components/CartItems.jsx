57c8441689718f411f8a5e6db5be3803356c0ff7
GAURAV YADAV <46158826+DVGY@users.noreply.github.com>
2020-08-17 10:46:24 +0530

    Trying to optimize Renders

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
-
 import DelSVG from '../assets/DelSVG';
 
 import { connect } from 'react-redux';
@@ -17,18 +16,52 @@ const CartItems = ({
   addItemCart,
   removeItemCart,
   deleteCart,
-  cartCount,
+  cartCount1,
+  cartCount2,
+  cartCount3,
+  cartCount4,
 }) => {
   return (
     <div className="cartbar-box__item d-flex mb-3 justify-content-center">
       {console.log('Rendering inside cartItems.jsx')}
-      <Badge
-        variant={cartCount[id].count > 0 ? 'primary' : 'warning'}
-        className="ml-1 ml-sm-3 "
-      >
-        {' '}
-        {cartCount[id].count}
-      </Badge>
+
+      {id === 'cartItem1' && (
+        <Badge
+          variant={cartCount1.count > 0 ? 'primary' : 'warning'}
+          className="ml-1 ml-sm-3 "
+        >
+          {' '}
+          {cartCount1.count}
+        </Badge>
+      )}
+      {id === 'cartItem2' && (
+        <Badge
+          variant={cartCount2.count > 0 ? 'primary' : 'warning'}
+          className="ml-1 ml-sm-3 "
+        >
+          {' '}
+          {cartCount2.count}
+        </Badge>
+      )}
+      {id === 'cartItem3' && (
+        <Badge
+          variant={cartCount3.count > 0 ? 'primary' : 'warning'}
+          className="ml-1 ml-sm-3 "
+        >
+          {' '}
+          {cartCount3.count}
+        </Badge>
+      )}
+      {id === 'cartItem4' && (
+        <Badge
+          variant={cartCount4.count > 0 ? 'primary' : 'warning'}
+          className="ml-1 ml-sm-3 "
+        >
+          {' '}
+          {cartCount4.count}
+        </Badge>
+      )}
+
       <Button
         onClick={() => addItemCart(id)}
         variant="secondary"
@@ -61,6 +94,9 @@ const mapDispatchToProps = (dispatch) => ({
   deleteCart: (id) => dispatch(deleteCart(id)),
 });
 const mapStateToProps = (state) => ({
-  cartCount: state.count,
+  cartCount1: state.count.cartItem1,
+  cartCount2: state.count.cartItem2,
+  cartCount3: state.count.cartItem3,
+  cartCount4: state.count.cartItem4,
 });
-export default React.memo(connect(mapStateToProps, mapDispatchToProps)(CartItems));
+export default connect(mapStateToProps, mapDispatchToProps)(CartItems);
